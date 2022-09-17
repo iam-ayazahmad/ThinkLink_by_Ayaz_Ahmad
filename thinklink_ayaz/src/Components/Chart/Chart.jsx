2a16a65de3721b1845008412631de5f45d45e2d0
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-09-18 01:28:33 +0530

    new

---
+++ thinklink_ayaz/src/Components/Chart/Chart.jsx
@@ -0,0 +1,85 @@
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import "../Chart/Chart.css"
+const data = [
+  {
+    name: '',
+    uv: 4000,
+    pv: 0,
+    amt: 2400,
+  },
+  {
+    name: 'MON',
+    uv: 5000,
+    pv: 5000,
+    amt: 2210,
+  },
+  {
+    name: 'TUE',
+    uv: 2000,
+    pv: 2500,
+    amt: 2290,
+  },
+  {
+    name: 'WED',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'THU',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'FRI',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'SAT',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: 'SUN',
+    uv: 3490,
+    pv: 2000,
+    amt: 2100,
+  },
+];
+
+export default class Example extends PureComponent {
+  
+
+  render() {
+    return (
+        <div className='mainchart'>
+      <ResponsiveContainer width="70%" height="80%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+        //   margin={{
+        //     top: 5,
+        //     right: 30,
+        //     left: 20,
+        //     bottom: 5,
+        //   }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line  dataKey="pv" stroke="#0e9e5b" activeDot={{ r: 5 }} />
+          
+        </LineChart>
+      </ResponsiveContainer>
+      </div>
+    );
+  }
+}
